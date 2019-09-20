0035d9fae30925c9c7478c58d99c387f7f87aff5
Randel Jiao <randeljiao@gmail.com>
2019-09-20 17:06:16 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,33 @@
-class Song
-  attr_accessor :name, :artist_name
-  @@all = []
 
-  def self.all
-    @@all
+
+class Song 
+  attr_accessor :name, :genre, :artist
+ 
+  def self.new_from_csv(csv_data)
+    # Split the CSV data into an array of individual rows.
+    rows = csv_data.split("\n")
+    # For each row, let's collect a Person instance based on the data
+    songs = rows.collect do |row|
+      
+      data = row.split(", ")
+      name = data[0]
+      genre = data[1]
+      artist = data[2]
+ 
+      # Make a new instance
+      Song = self.new # self refers to the Person class. This is song.new
+      
+      song.name = name
+      song.genre = genre
+      song.artist = artist
+      
+      song
+    end
+   
+    songs
   end
-
-  def save
-    self.class.all << self
+    def self.destroy_all
+    self.all.clear
   end
 
 end
